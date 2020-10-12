b4619b29bba5110b2cefbd1cd415e1fc0c1d3409
Manuel Soelch <manuel.soelch@bmw.de>
2020-10-12 17:20:19 +0200

    Implement getTodos.ts

--- backend/src/lambda/http/getTodos.ts
+++ backend/src/lambda/http/getTodos.ts
@@ -1,7 +1,29 @@
-// import 'source-map-support/register'
+/*
+import 'source-map-support/register'
 //
-// import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import * as AWS  from 'aws-sdk'
+
+const docClient = new AWS.DynamoDB.DocumentClient()
+const todoTable = process.env.TODOS_TABLE
 //
-// export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-//   // TODO: Get all TODO items for a current user
-// }
+export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+
+      console.log(event);
+
+      const toDoItem = await docClient.scan({
+                                    TableName: todoTable
+                                  }).promise();
+
+      return {
+          statusCode: 200,
+          headers: {
+            'Access-Control-Allow-Origin': '*'
+          },
+          body: JSON.stringify({
+            toDoItem
+          })
+        }
+
+}
+ */
