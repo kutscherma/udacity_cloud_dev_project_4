7ee96a55273bb0526b9d6846ad34813c2f6cd035
Judith Freiberger <judith.freiberger@bmw.de>
2020-10-20 01:38:10 +0200

    Change logger name

--- backend/src/lambda/http/generateUploadUrl.ts
+++ backend/src/lambda/http/generateUploadUrl.ts
@@ -1,10 +1,24 @@
-// import 'source-map-support/register'
-//
-// import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
-//
-// export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-//   const todoId = event.pathParameters.todoId
-//
-//   // TODO: Return a presigned URL to upload a file for a TODO item with the provided id
-//   return undefined
-// }
+import 'source-map-support/register'
+
+import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import { getUserId } from '../utils'
+import { getUploadUrl } from "../../services/todos";
+import { createLogger } from '../../utils/logger'
+const logger = createLogger('generateUploadUrl')
+
+export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    const todoId = event.pathParameters.todoId
+
+    const uploadUrl = await getUploadUrl(todoId, getUserId(event))
+    logger.info(`Upload URL: ${uploadUrl}`)
+
+    return {
+        statusCode: 201,
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Credentials': true,
+            'Access-Control-Request-Method': 'POST'
+        },
+        body: JSON.stringify({ uploadUrl })
+    }
+}
